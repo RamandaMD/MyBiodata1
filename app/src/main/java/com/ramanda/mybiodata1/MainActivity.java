facc81385bcb5cbc6019ab927b5730636749d269
RamandaMD <ramandamd@mhs.mdp.ac.id>
2023-02-22 10:43:32 +0700

    Initial Project
    First Commit

---
+++ app/src/main/java/com/ramanda/mybiodata1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ramanda.mybiodata1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
