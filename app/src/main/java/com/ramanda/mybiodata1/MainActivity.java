9085bc7946cc6f2bcf83068a41e0587213c303bb
restianreza <114054889+restianreza@users.noreply.github.com>
2023-03-01 10:01:49 +0700

    -
    -

--- app/src/main/java/com/ramanda/mybiodata1/MainActivity.java
+++ app/src/main/java/com/ramanda/mybiodata1/MainActivity.java
@@ -2,13 +2,43 @@ package com.ramanda.mybiodata1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Utilities utilities = new Utilities();
+    private TextView tvUsername;
+    private Button btnLogOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvUsername = findViewById(R.id.tv_username);
+        btnLogOut = findViewById(R.id.btn_logout);
+
+        if(!utilities.isLogin(this,"xUsername")){
+            Intent intent = new Intent(this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            String username = utilities.getPref(this,"xUsername");
+            tvUsername.setText(username);
+        }
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                utilities.setPref(MainActivity.this, "xUsername", null);
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
